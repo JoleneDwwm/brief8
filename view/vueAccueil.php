6b5c5fba5becc3eac7d22260a40269302852d6bf
JoleneDwwm <jolene.dwwm2022@gmail.com>
2023-03-01 11:20:01 +0100

    Added buttons in vueAccueil

--- view/vueAccueil.php
+++ view/vueAccueil.php
@@ -7,5 +7,6 @@
             <h2><?= $instrument['Nom'] ?></h2></a>
         </header>
             <p><?= $instrument['Prix'] ?> dollars</p>
+            <button type="button"><a href="<?= "index.php?action=panier&id=". $instrument['ID'] ?>">Ajouter au panier</a></button>
     </div>
 <?php endforeach; ?>
